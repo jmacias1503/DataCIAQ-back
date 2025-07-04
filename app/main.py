02c16bbb1e65cfc0dcc8684f5f1efc8a137ecb34
Emiliano Aquetzalli Obregon Reyes <eobregon05@alumnos.uaq.mx>
2025-07-04 09:46:09 -0600

    feat : add CORS

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.infrastructure.api.routers.sensor_router import router as sensor_router
 from app.infrastructure.config.database import get_db
 import uvicorn
@@ -7,6 +8,15 @@ app = FastAPI(
     title="Climate Sensor API",
     description="API for managing climate sensor records",
     version="1.0.0"
+)
+
+# Configuración de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # En producción, especifica los dominios permitidos
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Include routers
